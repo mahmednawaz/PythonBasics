156cdc638f28fb45be719bf8e757608e688d1754
Muhammad Ahmed Nawaz <link.ahmednawaz@gmail.com>
2024-03-12 10:23:40 +0500

    added practice file

---
+++ PracticeProject.py
@@ -0,0 +1,22 @@
+x="this is text variable"
+print(type(x))
+y=12
+print(type(y))
+z=3.2
+print(type(z))
+s={1,2,3}
+print(type(s))
+s=[1,2,3,4]
+print(type(s))
+s=(1,2,3,4)
+print(type(s))
+s={"name":"ahmed","roll number":1234}
+print(type(s))
+x = 98
+if x>20:
+    print("this is true  is greater than 20")
+
+for x in range(0,10):
+    print (x)
+print("this is git repository")
+print("this is commited")
